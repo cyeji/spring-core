5d2a7b93b44849598ec92c8b1ae093602254526c
yeji.jo <choyeji1591@gmail.com>
2023-05-08 21:36:03 +0900

    test : 기능 별 실행 코드 작성

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -2,16 +2,20 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemberService;
 
 public class MemberApp {
 
+
     public static void main(String[] args) {
-        MemberServiceImpl memberService = new MemberServiceImpl();
-        Member memberA = new Member(1L, "memberA", Grade.VIP);
-        memberService.join(memberA);
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println(findMember.equals(memberA));
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,22 +3,22 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
 
-    public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
 
-        Long memberId = 1L;
-        Member member = new Member(memberId,"memberA", Grade.VIP);
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        OrderService orderService = appConfig.orderService();
+        MemberService memberService = appConfig.memberService();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberAIP", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "cheese", 10000);
 
         System.out.println("order = " + order);
 
