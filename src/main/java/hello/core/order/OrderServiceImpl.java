4242f62f37529d4caa3bdb7310058e2fdd5e40ba
yeji.jo <choyeji1591@gmail.com>
2023-05-13 20:22:14 +0900

    feat : @Autowired 옵션 처리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -17,7 +17,7 @@ public class OrderServiceImpl implements OrderService {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
+     
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
